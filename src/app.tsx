0d830d37f0c36b6f5367b5bfd3d87a3ffd34c556
lidagen-dot <2463446836@qq.com>
2025-03-07 16:17:39 +0800

    删除pnpm-lock.yaml

--- src/app.tsx
+++ src/app.tsx
@@ -17,7 +17,6 @@ const NO_NEED_LOGIN_WHITELIST = ['/user/register', loginPath];
 
 
 
-
 export const request: RequestConfig = {
   //baseUrl不加端口8080可以
   baseURL:process.env.NODE_ENV === 'production' ? 'https://springboot-cy09-144378-5-1346809069.sh.run.tcloudbase.com' : undefined,
